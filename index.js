6af821ac6b06964d115c0f850f48535eb5743ecb
Hristo Todorov <hristo.todorov@imperiamobile.com>
2018-07-25 22:22:37 +0300

    fix heroku ports

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log('Example app listening on port 3000!'))
