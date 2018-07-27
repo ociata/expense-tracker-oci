4076ede4cdc6a9fb2e7d0c37f1410f65b82754ea
Hristo Todorov <hristo.todorov@imperiamobile.com>
2018-07-27 17:56:11 +0300

    start serving swagger docs from api-docs route

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./config/keys')
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./openapi.json')
 
 // import all schemas before connect to mongoose/mongodb
 require('./models/User')
@@ -12,6 +14,10 @@ const User = mongoose.model(model.USERS_MODEL)
 
 const app = express()
 
+// setup docs to be served at api-docs
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// todo: remove this when real endpoints are ready
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/newUser', (req, res) => {
