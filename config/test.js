3f876ba171f6f8e2eea53fdce1643a3ec1359abc
Hristo Todorov <hristo.todorov@imperiamobile.com>
2018-09-01 12:27:09 +0300

    execute tests againts dedicated staging db

---
+++ config/test.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://userstagingdb:EF%40EkA$$cr9K@ds020228.mlab.com:20228/expense-tracker-oci-stagging',
+  api_key: 'f516c055-7340-4687-8afb-537e481a356d',
+  token_secret: 'EF@EkA$$cr9K'
+}
