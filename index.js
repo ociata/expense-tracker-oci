a51ef100ffa7d9ff7c23cbfb90c701c7f1fb6ddb
Hristo Todorov <hristo.todorov@imperiamobile.com>
2018-09-01 11:14:42 +0300

    added first test(auth)

--- index.js
+++ index.js
@@ -36,3 +36,5 @@ require('./routes/session-routes')(app, googleClient)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log('Example app listening on port: ' + PORT))
+
+module.exports = app
---
+++ test/auth.js
@@ -0,0 +1,56 @@
+//During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+
+let server = require('../index')
+
+const config = require('../config/keys')
+
+let mongoose = require("mongoose")
+const model = require('../models/model-keys')
+const User = mongoose.model(model.USERS_MODEL)
+const InsecureSession = mongoose.model(model.INSECURE_SESSION_MODEL)
+
+//Require the dev-dependencies
+let chai = require('chai')
+let chaiHttp = require('chai-http')
+let should = chai.should()
+
+chai.use(chaiHttp)
+
+const refreshToken = "1/mnSXA-PbIO_WEdkd3tmCb9MjTkDw4NZe1184679jdoYmW-qt_-H3cetLFuHFF1_V"
+const googleId = "113582906180370887650"
+const name = "Hristo Todorov"
+const email = "oci@todorov.io"
+
+describe('Auth', () => {
+  beforeEach(async () => {
+    // prepare db before each test
+
+    // clear collections
+    await User.remove({})
+    await InsecureSession.remove({})
+
+    // store
+    await new User({ googleId, name, email}).save()
+    await new InsecureSession({ userReference: googleId, refreshToken }).save()
+  })
+
+  describe('POST /auth', () => {
+    it('should auth with expired token when security off', async () => {
+      console.log("2312")
+      
+      const expiredToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjQwYzZiMDliNDQ5NjczNDUzYzNkYTY5OWUyZGY1NTI3ZjkxZTY4MDMifQ.eyJhenAiOiI0MDAzOTk2Mzc2MDAtZ2s0N2xuOGYxcmdmbnIza2IxcjA3YmQ2bWI0YjFkdGwuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI0MDAzOTk2Mzc2MDAtZ2s0N2xuOGYxcmdmbnIza2IxcjA3YmQ2bWI0YjFkdGwuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTM1ODI5MDYxODAzNzA4ODc2NTAiLCJoZCI6InRvZG9yb3YuaW8iLCJlbWFpbCI6Im9jaUB0b2Rvcm92LmlvIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF0X2hhc2giOiJ3WDNNU0tKbnRzYjNnZ082b0dNOU5RIiwiZXhwIjoxNTMzNDc3MTcwLCJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJpYXQiOjE1MzM0NzM1NzAsIm5hbWUiOiJIcmlzdG8gVG9kb3JvdiIsInBpY3R1cmUiOiJodHRwczovL2xoNi5nb29nbGV1c2VyY29udGVudC5jb20vLXQwVVVJY1h0TWR3L0FBQUFBQUFBQUFJL0FBQUFBQUFBQUFBL0FBbm5ZN3Azc0hEMC1Uc0lieHZBRmFVRkVQOGpjeU9uLVEvczk2LWMvcGhvdG8uanBnIiwiZ2l2ZW5fbmFtZSI6IkhyaXN0byIsImZhbWlseV9uYW1lIjoiVG9kb3JvdiIsImxvY2FsZSI6ImVuIn0.KpXfyR-dM1uaoVNE6DPMQdZvul3lHl0l5InLpWdxKzPwnHOwY0k3paEpUNjy7o8zAihpOPlLq4C6Hi_ctbHvDFi0TbTLZLkhrvll4tHAaTIwm9gGamVdEfAeL_JAITCkC0VUfTmA_HXV3pkjZezI_1mmiavQyU9fQkT0asUUGXw5-RY8Cu_461eZFqrqhvG-8NvB5XRbTZFI5P5oKiqoCiR2jpKpTmsaQsBRaTFNSqpHXP5gtbCPRCwL1FKmFnjv_HZCP8FtMOdazGOSrbs-V9M7Akm773gzG0-Lt_vpes14kj5kmrLa-G9JV1Ye6006g2OeegkUGl8RV_TkaPr9QQ"
+
+      chai.request(server)
+        .post(`/auth?googleToken=${expiredToken}&googleId=${googleId}`)
+        .set('Content-Type', 'application/json')
+        .set('api_key', config.api_key)
+        .send()
+        .end((err, res) => {
+          res.should.have.status(200)
+          res.body.should.have.property('userId')
+          res.body.should.have.property('googleId').eql(googleId)
+        })
+    })
+  })
+})
